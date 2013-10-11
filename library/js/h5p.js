fe0a83e06e0938f891d2660393f47e1bf30effe3
Frode Petterson <frode.petterson@amendor.com>
2013-10-11 10:29:04 +0200

    Skip setFinished for Drupal.

--- library/js/h5p.js
+++ library/js/h5p.js
@@ -311,6 +311,7 @@ H5P.shuffleArray = function(array) {
  * @param {Number} maxPoints
  */
 H5P.setFinished = function (contentId, points, maxPoints) {
+  return; // Not yet implemented for Drupal!
   if (H5P.postUserStatistics === true) {
     H5P.jQuery.post(H5P.ajaxPath + 'setFinished', {contentId: contentId, points: points, maxPoints: maxPoints});
   }
