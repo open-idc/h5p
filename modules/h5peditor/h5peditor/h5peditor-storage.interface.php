cdde15fdd7d88aedc950742a6891fe941536985c
Frode Petterson <frode.petterson@amendor.com>
2014-01-07 10:17:24 +0100

    Added support for reading libraries from dev folder. Almost done adding support for editor reading libs from dev folder. Some minor fixes to improve readability and performance.

--- modules/h5peditor/h5peditor/h5peditor-storage.interface.php
+++ modules/h5peditor/h5peditor/h5peditor-storage.interface.php
@@ -4,7 +4,7 @@
 
 interface H5peditorStorage {
   public function getSemantics($machine_name, $major_version, $minor_version);
-  public function addTempFile($file);
+  public function addTmpFile($file);
   public function removeFile($path);
   public function keepFile($oldPath, $newPath);
   public function getLibraries();
