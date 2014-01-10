f4ed4e8f1c35d00ec89f6576ac2ac6f8a69764ae
Pål Jørgensen <pal.jorgensen@gmail.com>
2014-01-10 13:51:54 +0100

    Added list view of libraries

---
+++ library/js/h5p-library-list.js
@@ -0,0 +1,47 @@
+var H5PLibraryList= H5PLibraryList || {};
+
+(function ($) {
+
+  /**
+   * Initializing
+   */
+  H5PLibraryList.init = function () {
+    var $adminContainer = H5PIntegration.getAdminContainer();
+    $adminContainer.append(H5PLibraryList.createLibraryList(H5PIntegration.getLibraryList()));
+  };
+  
+  /**
+   * 
+   * 
+   * @param {object} libraries List of libraries and headers
+   */
+  H5PLibraryList.createLibraryList = function (libraries) {
+    // Create table
+    var $table = H5PUtils.createTable(libraries.listHeaders);
+    
+    // Add libraries
+    $.each (libraries.listData, function (index, library) {
+      var $libraryRow = H5PUtils.createTableRow(library);
+      
+      // Open details view when clicked
+      $libraryRow.on('click', function (){
+        var endpoints = H5PIntegration.getAdminEndpoints();
+        window.location.href = endpoints.libraryDetails + library.machineName;
+      });
+      
+      $table.append($libraryRow);
+    });
+    
+    return $table;
+  };
+ 
+  
+  // Initialize me:
+  $(document).ready(function () {
+    if (!H5PLibraryList.initialized) {
+      H5PLibraryList.initialized = true;
+      H5PLibraryList.init();
+    }
+  });
+  
+})(H5P.jQuery);
