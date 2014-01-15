eb440441651efbbed89dd055e8f9e7f501e56a45
Pål Jørgensen <pal.jorgensen@gmail.com>
2014-01-15 12:53:13 +0100

    HFJ-115: list content belonging to library

--- library/js/h5p-utils.js
+++ library/js/h5p-utils.js
@@ -8,14 +8,19 @@ var H5PUtils = H5PUtils || {};
    */
   H5PUtils.createTable = function (headers) {
     var $table = $('<table class="h5p-admin-table"></table>');
-    var $thead = $('<thead></thead>');
-    var $tr = $('<tr></tr>');
-
-    $.each(headers, function (index, value) {
-      $tr.append('<th>' + value + '</th>');
-    });
     
-    return $table.append($thead.append($tr));
+    if(headers) {
+      var $thead = $('<thead></thead>');
+      var $tr = $('<tr></tr>');
+  
+      $.each(headers, function (index, value) {
+        $tr.append('<th>' + value + '</th>');
+      });
+      
+      $table.append($thead.append($tr))
+    }
+    
+    return $table;
   };
   
   /**
@@ -32,5 +37,33 @@ var H5PUtils = H5PUtils || {};
     
     return $tr;
   };
+  
+  /**
+   * Generic function for creating a field containing label and value
+   * 
+   * @param {string} label The label displayed in front of the value 
+   * @param {string} value The value
+   */
+  H5PUtils.createLabeledField = function (label, value) {
+    var $field = $('<div class="h5p-labeled-field"></div>');
+    
+    $field.append('<div class="h5p-label">' + label + '</div>');
+    $field.append('<div class="h5p-value">' + value + '</div>');
+    
+    return $field;
+  }
+  
+  /**
+   * Replaces placeholder fields in translation strings
+   * 
+   * @param {string} template The translation template string in the following format: "$name is a $sex"
+   * @param {array} replacors An js object with key and values. Eg: {'$name': 'Frode', '$sex': 'male'}
+   */
+  H5PUtils.translateReplace = function (template, replacors) {
+    $.each(replacors, function (key, value) {
+      template = template.replace(new RegExp('\\'+key, 'g'), value)
+    });
+    return template;
+  }
   
 })(H5P.jQuery);
