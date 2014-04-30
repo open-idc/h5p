c15d32b507ef68cb35b01c7384437e2b4ee5b6ee
Frode Petterson <frode.andre.petterson@gmail.com>
2014-04-30 16:04:52 +0200

    Imported changes from h5p-php-library

--- modules/h5peditor/h5peditor/scripts/h5peditor-library.js
+++ modules/h5peditor/h5peditor/scripts/h5peditor-library.js
@@ -121,7 +121,7 @@ ns.Library.prototype.loadLibrary = function (libraryName, preserveParams) {
       // Reset params
       that.params.params = {};
     }
-
+    
     ns.processSemanticsChunk(semantics, that.params.params, that.$libraryWrapper.html(''), that);
 
     if (that.libraries !== undefined) {
