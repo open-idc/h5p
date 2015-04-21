3ba652e378757fbbf6e8f005d6e9839db78f8e7c
Svein-Tore Griff With <s.t.g.with@gmail.com>
2015-04-21 16:55:48 +0200

    Update library

--- library/js/h5p-resizer.js
+++ library/js/h5p-resizer.js
@@ -1,8 +1,9 @@
 // H5P iframe Resizer
 (function () {
-  if (!window.postMessage || !window.addEventListener) {
+  if (!window.postMessage || !window.addEventListener || window.h5pResizerInitialized) {
     return; // Not supported
   }
+  window.h5pResizerInitialized = true;
 
   // Map actions to handlers
   var actionHandlers = {};
@@ -46,7 +47,7 @@
    */
   actionHandlers.prepareResize = function (iframe, data, respond) {
     responseData = {};
-    
+
     // Create spaceholder and insert after iframe.
     var spaceholder = document.createElement('div');
     spaceholder.style.height = (iframe.clientHeight - 1) + 'px';
@@ -69,7 +70,7 @@
   actionHandlers.resize = function (iframe, data, respond) {
     // Resize iframe so all content is visible.
     iframe.style.height = data.height + 'px';
-    iframe.nextSibling.remove();
+    iframe.parentNode.removeChild(iframe.nextSibling);
   };
 
   /**
