a337736c6982bc17807934bb761e168f92f6c9e9
Tom Arild Jakobsen <tajakobsen@gmail.com>
2017-05-25 13:04:10 +0200

    Add caching for content in iframes
    Use the JsCollectionOptimizer and CssCollection optimizer to create
    concatinated and cached versions of the files loaded.
    
    HFP-1114

--- src/Plugin/Field/FieldFormatter/H5PDefaultFormatter.php
+++ src/Plugin/Field/FieldFormatter/H5PDefaultFormatter.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\H5P\Plugin\Field\FieldFormatter;
 
+use Drupal\Core\Asset\AssetCollectionOptimizerInterface;
 use Drupal\Core\Field\FormatterBase;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\h5p\H5PDrupal\H5PDrupal;
@@ -40,6 +41,8 @@ class H5PDefaultFormatter extends FormatterBase {
    */
   public function viewElements(FieldItemListInterface $items, $langcode) {
     $element = array();
+    $jsOptimizer = \Drupal::service('asset.js.collection_optimizer');
+    $cssOptimizer = \Drupal::service('asset.css.collection_optimizer');
 
     foreach ($items as $delta => $item) {
       $value = $item->getValue();
@@ -81,13 +84,19 @@ class H5PDefaultFormatter extends FormatterBase {
       else {
         $html = '<div class="h5p-iframe-wrapper"><iframe id="h5p-iframe-' . $h5p_content->id() . '" class="h5p-iframe" data-content-id="' . $h5p_content->id() . '" style="height:1px" src="about:blank" frameBorder="0" scrolling="no"></iframe></div>';
 
-        // Get core components needed to display content
-        $h5p_integration['core'] = H5PDrupal::getCoreAssets();
+        // Load core assets
+        $coreAssets = H5PDrupal::getCoreAssets();
+
+        $h5p_integration['core']['styles'] = $this->createCachedPublicFiles($coreAssets['styles'], $cssOptimizer);
+        $h5p_integration['core']['scripts'] = $this->createCachedPublicFiles($coreAssets['scripts'], $jsOptimizer);
 
         // Load dependencies
         $files = $core->getDependenciesFiles($preloaded_dependencies);
-        $h5p_integration['contents'][$content_id_string]['scripts'] = $core->getAssetsUrls($files['scripts']);
-        $h5p_integration['contents'][$content_id_string]['styles'] = $core->getAssetsUrls($files['styles']);
+        $jsFilePaths = $this->getDependencyFilePaths($files['scripts'], $h5p_integration['url']);
+        $cssFilePaths = $this->getDependencyFilePaths($files['styles'], $h5p_integration['url']);
+
+        $h5p_integration['contents'][$content_id_string]['styles'] = $this->createCachedPublicFiles($cssFilePaths, $cssOptimizer);
+        $h5p_integration['contents'][$content_id_string]['scripts'] = $this->createCachedPublicFiles($jsFilePaths, $jsOptimizer);
       }
 
       // Render each element as markup.
@@ -114,4 +123,62 @@ class H5PDefaultFormatter extends FormatterBase {
     return $element;
   }
 
+  /**
+   * Takes files configs and returns absolute file paths
+   *
+   * @param array $scripts
+   * @param string $baseUrl
+   *
+   * @return string[]
+   */
+  private function getDependencyFilePaths(array $scripts, $baseUrl) {
+    return preg_filter('/^/', ltrim($baseUrl, '/'), array_column($scripts, 'path'));
+  }
+
+  /**
+   * Combines a set of files to a cached version, that is public available
+   *
+   * @param string[] $filePaths
+   * @param AssetCollectionOptimizerInterface $optimizer
+   *
+   * @return string[]
+   */
+  private function createCachedPublicFiles(array $filePaths, $optimizer) {
+    $assets = $this->createDependencyFileAssets($filePaths);
+    $cachedAsset = $optimizer->optimize($assets);
+
+    return array_map(function($publicUrl){ return file_create_url($publicUrl); }, array_column($cachedAsset, 'data'));
+  }
+
+  /**
+   * Takes a list of file paths, and creates drupal Assets
+   *
+   * @param string[] $filePaths
+   * @return array
+   */
+  private function createDependencyFileAssets($filePaths) {
+    $result = array();
+
+    foreach ($filePaths as $index => $path) {
+      $path = $this->cleanFilePath($path);
+
+      $result[$path] = [
+        'type' => 'file',
+        'group' => 'h5p',
+        'weight' => count($filePaths) - $index,
+        'cache' => TRUE,
+        'preprocess' => TRUE,
+        'attributes' => [],
+        'version' => NULL,
+        'browsers' => [],
+        'data' => $path
+      ];
+    }
+
+    return $result;
+  }
+
+  private function cleanFilePath($path){
+    return explode('?', ltrim($path, '/'))[0];
+  }
 }
