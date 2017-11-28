db0610f70d7c7bfbb65623498604cf5b95293136
Frode Petterson <frode.andre.petterson@gmail.com>
2017-11-28 14:18:31 +0100

    Update composer requirements

--- vendor/h5p/h5p-core/h5p.classes.php
+++ vendor/h5p/h5p-core/h5p.classes.php
@@ -2509,7 +2509,9 @@ class H5PCore {
       'h5p_version' => $platform['h5pVersion'],
       'disabled' => $fetchingDisabled ? 1 : 0,
       'local_id' => hash('crc32', $this->fullPluginPath),
-      'type' => $this->h5pF->getOption('site_type', 'local')
+      'type' => $this->h5pF->getOption('site_type', 'local'),
+      'core_api_version' => H5PCore::$coreApi['majorVersion'] . '.' .
+                            H5PCore::$coreApi['minorVersion']
     );
 
     // Register site if it is not registered
