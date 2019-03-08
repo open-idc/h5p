379b0614d9aad89bd04c36e909e664c9e00f80fb
Frode Petterson <frode.andre.petterson@gmail.com>
2019-03-08 14:36:13 +0100

    Fix isEmpty check

--- src/Plugin/Field/FieldType/H5PItem.php
+++ src/Plugin/Field/FieldType/H5PItem.php
@@ -65,7 +65,7 @@ class H5PItem extends FieldItemBase implements FieldItemInterface {
    * {@inheritdoc}
    */
   public function isEmpty() {
-    return empty($this->get('h5p_content_id')->getValue());
+    return (empty($this->values['h5p_content']) || empty($this->values['h5p_content']['library'])) && empty($this->get('h5p_content_id')->getValue());
   }
 
   /**
